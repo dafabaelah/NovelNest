e1722aa9cc0ce4c431e44fde107f474e9982d1cf
MutiaraLF26 <mutiara.213040079@mail.unpas.ac.id>
2023-12-21 23:17:58 +0700

    membenarkan navbar d halaan home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,140 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite(['resources/css/app.css','resources/js/app.js'])
-  <title>NovelNest</title>
-</head>
-<body>
-
-<section class="h-screen bg-cover bg-no-repeat bg-center bg-gray-800 bg-blend-multiply" style="background-image: url('images/bg.jpeg');">
-  <div>
-  <nav class="bg-amber-">
-  <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-    <div class="relative flex h-16 items-center justify-between ">
-      <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-        <!-- Mobile menu button-->
-        <button type="button" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
-          <span class="absolute -inset-0.5"></span>
-          <span class="sr-only">Open main menu</span>
-          <!--
-            Icon when menu is closed.
-
-            Menu open: "hidden", Menu closed: "block"
-          -->
-          <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-          </svg>
-          <!--
-            Icon when menu is open.
-
-            Menu open: "block", Menu closed: "hidden"
-          -->
-          <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-          </svg>
-        </button>
+@extends('templates.main')
+@section('container')
+ <!-- About Section Start -->
+ <section id="about" class=" h-screen bg-cover bg-no-repeat bg-center bg-gray-800 bg-blend-multiply" style="background-image: url('images/bg.jpeg');">
+  <div class="container">
+    <div class="mx-10  max-w-lg  text-white ">
+      <h1 class="font-semibold text-4xl">Selamat Datang di Website NovelNest</h1>
+      <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, et nihil! Eligendi minus nulla accusamus, 
+        ducimus mollitia asperiores libero ea cum ullam nihil eius incidunt dolorum non laborum quia voluptatum.</P>
       </div>
-      <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-        <div class="flex flex-shrink-0 items-center">
-          <img class="w-10 h-10 rounded-full"src="images/logo.png?color=indigo&shade=700" alt="">
-        </div> 
-        <div class="hidden sm:ml-6 sm:block">
-          <div class="flex space-x-4">
-            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-            <a href="#" class="text-gray-300 hover:bg-gray-700 rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Genre</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Best Seller</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Rekomendasi</a>
-          </div>
-        </div>
-      </div>
-      
-
-      <!-- Profile dropdown -->
-      <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
-            <span class="sr-only">Open user menu</span>
-            <img class="w-8 h-8 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-3.jpg" alt="user photo">
-          </button>
-          <!-- Dropdown menu -->
-          <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
-            
-            <ul class="py-2" aria-labelledby="user-menu-button">
-              <li class="flex-1 items-center">
-                <a href="/login" class="flex items-center px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"><svg class="w-3 h-3 mr-2 mt-1 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 15">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 7.5h11m0 0L8 3.786M12 7.5l-4 3.714M12 1h3c.53 0 1.04.196 1.414.544.375.348.586.82.586 1.313v9.286c0 .492-.21.965-.586 1.313A2.081 2.081 0 0 1 15 14h-3"/>
-                </svg>Sign In</a>
-              </li>
-            </ul>
-          </div>
-          <button data-collapse-toggle="navbar-user" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
-            <span class="sr-only">Open main menu</span>
-            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-            </svg>
-        </button>
-      </div>
-
-      </div>
-    </div>
-  </div>
-
-  <!-- Mobile menu, show/hide based on menu state. -->
-  <div class="sm:hidden" id="mobile-menu">
-    <div class="space-y-1 px-2 pb-3 pt-2">
-      <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-      <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">Dashboard</a>
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Team</a>
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Calendar</a>
-    </div>
-  </div>
-</nav>
-
-    <div class="mx-10 mt-40 max-w-lg  text-white ">
-    <h1 class="font-semibold text-4xl">Selamat Datang di Website NovelNest</h1>
-    <P>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, et nihil! Eligendi minus nulla accusamus, 
-      ducimus mollitia asperiores libero ea cum ullam nihil eius incidunt dolorum non laborum quia voluptatum.</P>
-    </div>
-
-    <div class="mx-10 mt-5">
-      <a href="#" class="bg-blue-400 rounded-3xl py-3 px-8 font-medium text-white">Mulai Membaca</a>
-    </div>
-
-    <div>
-      <img src="images/novel.png" class="w-full xl:w-1/3 xl:absolute right-20 bottom-0">
-    </div>
-
-  </div>
-  </section>
-
-  <!-- About Section Start -->
-      <section id="about" class="h-screen bg-cover bg-no-repeat bg-center bg-gray-800 bg-blend-multiply" style="background-image: url('images/bg.jpeg');">
-        <div class="container">
-          <div class="flex items-center justify-center h-full">
-            <div class="w-full px-40 mb-auto py-20">
-              <h4 class="font-bold uppercase text-white text-lg mb-3">Tentang NovelNest</h4>
-              <h2 class="font-blod text-white text-3xl mb-3 max-w-md lg:text-4xl">Membaca novel di NovelNest</h2>
-              <p class="font-medium text-base text-white max-w-xl lg:text-lg">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, ipsam cumque. 
-                Hic quo quibusdam fugiat illum adipisci unde enim, totam non nemo fugit in qui, 
-                error deserunt? Harum, suscipit qui!</p>
-            </div>
-
-            <div>
-              <img src="images/novel.png" class="w-full xl:w-1/3 xl:absolute right-20 bottom-0">
-            </div>
-          </div>
-        </div>
-      </section>
-
-  <!-- About Section End -->
-
   
+      <div class="mx-10 mt-5">
+        <a href="#" class="bg-blue-400 rounded-3xl py-3 px-8 font-medium text-white">Mulai Membaca</a>
+      </div>
+  
+      <div>
+        <img src="images/novel.png" class="w-full xl:w-1/3 xl:absolute right-20 bottom-0">
+      </div>
+    </div>
+    <div class="flex items-center justify-center h-full">
+      <div class="w-full px-40 mb-auto py-20">
+        <h4 class="font-bold uppercase text-white text-lg mb-3">Tentang NovelNest</h4>
+        <h2 class="font-blod text-white text-3xl mb-3 max-w-md lg:text-4xl">Membaca novel di NovelNest</h2>
+        <p class="font-medium text-base text-white max-w-xl lg:text-lg">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, ipsam cumque. 
+          Hic quo quibusdam fugiat illum adipisci unde enim, totam non nemo fugit in qui, 
+          error deserunt? Harum, suscipit qui!</p>
+      </div>
 
-  <script src="https://cdn.tailwindcss.com"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
-</body>
-</html>
+      <div>
+        <img src="images/novel.png" class="w-full xl:w-1/3 xl:absolute right-20 bottom-0">
+      </div>
+    </div>
+  </div>
+
+</section>
+
+<!-- About Section End -->
+
+@endsection
