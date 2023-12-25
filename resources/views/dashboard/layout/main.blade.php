4df96bed88de3d00ff8659b486917274804e69c4
MutiaraLF26 <mutiara.213040079@mail.unpas.ac.id>
2023-12-25 14:58:36 +0700

    migrate database

--- resources/views/dashboard/layout/main.blade.php
+++ resources/views/dashboard/layout/main.blade.php
@@ -11,7 +11,7 @@
     <div class="min-h-full">
         @include('dashboard.layout.navbar')
         <main>
-            <div class="container mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <div>
                 @yield('container')
             </div>
         </main>
