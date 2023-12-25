dc94f833afed7da711a1c4fd9447660ed9e554c3
rikawenda <febriyantirika081@gmail.com>
2023-12-25 17:55:30 +0700

    Menyelesaikan halaman favorite

--- resources/views/dashboard/user/favorite/index.blade.php
+++ resources/views/dashboard/user/favorite/index.blade.php
@@ -4,7 +4,7 @@
     <div class="flex flex-wrap justify-center">
         <div class="max-w-sm mx-2 my-4 bg-white shadow-md rounded-lg overflow-hidden relative">
             <!-- Label Juara 1 Favorite -->
-            <span class="bg-blue-500 text-white flex mt-1 absolute top-2 left-2 px-2 py-1 rounded-full bg-opacity-100">#1</span>
+            <span class="bg-yellow-500 text-white flex mt-1 absolute top-2 left-2 px-2 py-1 rounded-full bg-opacity-100">#1</span>
 
             <img class="w-full h-64 object-cover object-center" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt="Product Image">
         
@@ -51,16 +51,15 @@
                     </div>
                     <span class="text-gray-500 ml-auto">123 Reviews</span>
                 </div>
-        
                 <div class="mt-4 flex items-center justify-between">
-                    <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Lanjutkan Baca</button>
-                    <button class="text-gray-300 hover:text-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm">Tambah ke Favorite</button>
+                    <button class="text-white bg-yellow-600 hover:bg-yellow-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Lanjutkan Baca</button>
+                    <button class="text-yellow-500 hover:text-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm">Tambah ke Favorite</button>
                 </div>
             </div>
         </div>
         <div class="max-w-sm mx-2 my-4 bg-white shadow-md rounded-lg overflow-hidden relative">
             <!-- Label Juara 1 Favorite -->
-            <span class="bg-blue-500 bg-opacity-50 text-white flex mt-1 absolute top-2 left-2 px-2 py-1 rounded-full">#2</span>
+            <span class="bg-yellow-500 bg-opacity-50 text-white flex mt-1 absolute top-2 left-2 px-2 py-1 rounded-full">#2</span>
 
 
             <img class="w-full h-64 object-cover object-center" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt="Product Image">
@@ -110,14 +109,14 @@
                 </div>
         
                 <div class="mt-4 flex items-center justify-between">
-                    <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Lanjutkan Baca</button>
-                    <button class="text-gray-300 hover:text-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm">Tambah ke Favorite</button>
+                    <button class="text-white bg-yellow-600 hover:bg-yellow-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Lanjutkan Baca</button>
+                    <button class="text-yellow-500 hover:text-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm">Tambah ke Favorite</button>
                 </div>
             </div>
         </div>
         <div class="max-w-sm mx-2 my-4 bg-white shadow-md rounded-lg overflow-hidden relative">
             <!-- Label Juara 1 Favorite -->
-            <span class="bg-blue-500 bg-opacity-50 text-white flex mt-1 absolute top-2 left-2 px-2 py-1 rounded-full">#3</span>
+            <span class="bg-yellow-500 bg-opacity-50 text-white flex mt-1 absolute top-2 left-2 px-2 py-1 rounded-full">#3</span>
 
 
             <img class="w-full h-64 object-cover object-center" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt="Product Image">
@@ -167,14 +166,14 @@
                 </div>
         
                 <div class="mt-4 flex items-center justify-between">
-                    <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Lanjutkan Baca</button>
-                    <button class="text-gray-300 hover:text-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm">Tambah ke Favorite</button>
+                    <button class="text-white bg-yellow-600 hover:bg-yellow-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Lanjutkan Baca</button>
+                    <button class="text-yellow-500 hover:text-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm">Tambah ke Favorite</button>
                 </div>
             </div>
         </div>
         <div class="max-w-sm mx-2 my-4 bg-white shadow-md rounded-lg overflow-hidden relative">
             <!-- Label Juara 1 Favorite -->
-            <span class="bg-blue-500 bg-opacity-50 text-white flex mt-1 absolute top-2 left-2 px-2 py-1 rounded-full">#4</span>
+            <span class="bg-yellow-500 bg-opacity-50 text-white flex mt-1 absolute top-2 left-2 px-2 py-1 rounded-full">#4</span>
 
 
             <img class="w-full h-64 object-cover object-center" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt="Product Image">
@@ -224,14 +223,14 @@
                 </div>
         
                 <div class="mt-4 flex items-center justify-between">
-                    <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Lanjutkan Baca</button>
-                    <button class="text-gray-300 hover:text-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm">Tambah ke Favorite</button>
+                    <button class="text-white bg-yellow-600 hover:bg-yellow-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Lanjutkan Baca</button>
+                    <button class="text-yellow-500 hover:text-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm">Tambah ke Favorite</button>
                 </div>
             </div>
         </div>
         <div class="max-w-sm mx-2 my-4 bg-white shadow-md rounded-lg overflow-hidden relative">
             <!-- Label Juara 1 Favorite -->
-            <span class="bg-blue-500 bg-opacity-50 text-white flex mt-1 absolute top-2 left-2 px-2 py-1 rounded-full">#5</span>
+            <span class="bg-yellow-500 bg-opacity-50 text-white flex mt-1 absolute top-2 left-2 px-2 py-1 rounded-full">#5</span>
 
 
             <img class="w-full h-64 object-cover object-center" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt="Product Image">
@@ -281,14 +280,14 @@
                 </div>
         
                 <div class="mt-4 flex items-center justify-between">
-                    <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Lanjutkan Baca</button>
-                    <button class="text-gray-300 hover:text-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm">Tambah ke Favorite</button>
+                    <button class="text-white bg-yellow-600 hover:bg-yellow-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Lanjutkan Baca</button>
+                    <button class="text-yellow-500 hover:text-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm">Tambah ke Favorite</button>
                 </div>
             </div>
         </div>
         <div class="max-w-sm mx-2 my-4 bg-white shadow-md rounded-lg overflow-hidden relative">
             <!-- Label Juara 1 Favorite -->
-            <span class="bg-blue-500 bg-opacity-50 text-white flex mt-1 absolute top-2 left-2 px-2 py-1 rounded-full">#6</span>
+            <span class="bg-yellow-500 bg-opacity-50 text-white flex mt-1 absolute top-2 left-2 px-2 py-1 rounded-full">#6</span>
 
 
             <img class="w-full h-64 object-cover object-center" src="https://flowbite.com/docs/images/blog/image-4.jpg" alt="Product Image">
@@ -338,11 +337,36 @@
                 </div>
         
                 <div class="mt-4 flex items-center justify-between">
-                    <button class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Lanjutkan Baca</button>
-                    <button class="text-gray-300 hover:text-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm">Tambah ke Favorite</button>
+                    <button class="text-white bg-yellow-600 hover:bg-yellow-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Lanjutkan Baca</button>
+                    <button class="text-yellow-500 hover:text-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm">Tambah ke Favorite</button>
                 </div>
             </div>
         </div>
+        <nav aria-label="Page navigation example">
+            <ul class="inline-flex -space-x-px text-base h-10">
+                <li>
+                <a href="#" class="flex items-center justify-center px-4 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+                </li>
+                <li>
+                <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+                </li>
+                <li>
+                <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+                </li>
+                <li>
+                <a href="#" aria-current="page" class="flex items-center justify-center px-4 h-10 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+                </li>
+                <li>
+                <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+                </li>
+                <li>
+                <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
+                </li>
+                <li>
+                <a href="#" class="flex items-center justify-center px-4 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 
 @endsection
