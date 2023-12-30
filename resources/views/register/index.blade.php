2fd9ba8b49d44e95e2dde113dec6be18ec19e34f
MutiaraLF26 <mutiara.213040079@mail.unpas.ac.id>
2023-12-30 22:00:18 +0700

    membenarkan halaman home

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -10,13 +10,13 @@
 </head>
 <body>
 <section class="h-screen bg-cover bg-no-repeat bg-center bg-blend-multiply" style="background-image: url('images/bgLoginRegister.jpg');">
-<div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+<div class="flex min-h-full flex-col justify-center px-2 py-2 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
   <img class="mx-auto h-20 w-auto" src="images/logoLoginRegister.jpg" alt="Logo">
     <h2 class="mt-2 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900">Register</h2>
   </div>
 
-  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+  <div class="mt-2 sm:mx-auto sm:w-full sm:max-w-sm">
     <form class="space-y-6" action="{{ route('store') }}" method="post">
       @csrf
       <div>
@@ -61,7 +61,7 @@
       </div>
     </form>
 
-    <p class="mt-10 text-center text-sm text-gray-500">
+    <p class="mt-2 text-center text-sm text-gray-500">
       Already have an account?
       <a href="/login" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Please, Login Here!</a>
     </p>
